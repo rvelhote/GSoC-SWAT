d347c2e604f19ebf020172420f5777c2f3e00537
Ricardo Velhote <rvelhote@gmail.com>
2009-06-06 14:18:19 +0100

    add: login controller

---
+++ swat/tests/functional/test_login.py
@@ -0,0 +1,7 @@
+from swat.tests import *
+
+class TestLoginController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='login', action='index'))
+        # Test response...
