fcffd107b3637e5160d88c9d1f351d7000c10001
Ricardo Velhote <rvelhote@gmail.com>
2009-06-06 14:17:28 +0100

    add: gitignore for the Komodo Edit project file and .pyc add: default root points to the dashboard controller. the controller doesn't exist yet but it will in the next commit. rm: cleanup public directory

--- swat/config/routing.py
+++ swat/config/routing.py
@@ -19,6 +19,11 @@ def make_map():
     map.connect('/error/{action}/{id}', controller='error')
 
     # CUSTOM ROUTES HERE
+    
+    #
+    #   Default root for Application
+    #
+    map.connect('/', controller='dashboard', action='index')
 
     map.connect('/{controller}/{action}')
     map.connect('/{controller}/{action}/{id}')
