323aaed88cefaa056b3597a7fab140f03822066e
Ricardo Velhote <rvelhote@gmail.com>
2009-07-01 23:10:26 +0100

    add: extra package requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,8 @@ setup(
     url='',
     install_requires=[
         "Pylons>=0.9.7",
+	"PyYAML>=3.0.8",
+	"Pam>=0.1.3"
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
