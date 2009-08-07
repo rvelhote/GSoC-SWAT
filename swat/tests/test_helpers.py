7a1f4c56344dc62d0d67136267af0add12a616b3
Ricardo Velhote <rvelhote@gmail.com>
2009-08-07 11:21:03 +0100

    change: redoing all tests add: test yaml files directory

--- swat/tests/test_helpers.py
+++ swat/tests/test_helpers.py
@@ -14,88 +14,101 @@
 # You should have received a copy of the GNU General Public License
 # 
 from swat.lib.helpers import *
+from pylons import config
 
-class TestMenu():
-    def test_get_menu_empty_type(self):
-	type = ""
-	assert len(get_menu(type)) == 0, 'When there is no type it should return an empty list'
-
-    def test_get_menu_valid_type(self):
-	type = "top"
-	assert len(get_menu(type)) > 0, 'Length of the  returned list should be > 0'
-
-    def test_get_menu_invalid_type(self):
-	type = "vandelayindustries"
-	assert len(get_menu(type)) == 0, 'When the menu doesn\'t exist an empty list should be returned'
-
-class TestSambaServerStatus():
-    def test_get_samba_server_status(self):
-	status = get_samba_server_status()
-        assert status == "up" or status == "down", 'Status should always be "up" or "down"'
-
-class TestDashboardConfiguration():
+class TestMenuConfiguration():
     def setUp(self):
-	self.d = DashboardConfiguration()
+	self.phony_instance = MenuConfiguration("topaaaaaaa", config['yaml.testfiles'])
+	self.instance = MenuConfiguration("top", config['yaml.testfiles'])
 
-    def tearDown(self):
-	pass
+    def test_get_phony_menu_items(self):
+	items = self.phony_instance.get_items()
+	assert len(items) == 0, 'Non-existing menu should return empty dictionary'
+	
+    def test_get_menu_items(self):
+	items = self.instance.get_items()
+	assert len(items) > 0, 'Existing menu should return at least one item'
+    
+    def test_loop_through(self):
+	items = self.instance.get_items()
+    
+#
+#    def test_get_menu_valid_type(self):
+#	type = "top"
+#	assert len(get_menu(type)) > 0, 'Length of the  returned list should be > 0'
+#
+#    def test_get_menu_invalid_type(self):
+#	type = "vandelayindustries"
+#	assert len(get_menu(type)) == 0, 'When the menu doesn\'t exist an empty list should be returned'
 
-    def test_load_config(self):
-	self.d.load_config('index')
-	assert len(self.d.get_layout()) > 0
-
-class TestSwatMessages():
-    def setUp(self):
-	self.m = SwatMessages()
-
-    def tearDown(self):
-	pass
-	
-    def test_add(self):
-	self.m.add('test message', 'cool')
-	assert self.m.any() == True
-	
-    def test_clear(self):
-	self.m.add('test message', 'cool')
-	self.m.clean()
-	assert self.m.any() == False
-	
-    def test_any(self):
-	self.m.add('test message', 'cool')
-	assert self.m.any() == True
-	
-	self.m.clean()
-	assert self.m.any() == False
-	
-class TestBreadcrumbTrail():
-    def setUp(self):
-	self.c = ControllerConfiguration('share')	
-	self.b = self.new_instance()
-
-    def new_instance(self):
-	return BreadcrumbTrail(self.c)
-	
-    def test_add(self):
-	self.b = self.new_instance()
-	self.b.add('bread', 'share', 'index')
-	
-	items = self.b.get()
-	assert len(items) > 0
-	
-    def test_build(self):
-	self.b = self.new_instance()
-	self.b.build()
-	
-	items = self.b.get()
-	
-	assert len(items) > 0
-		
-	for i in items:
-	    assert i.has_key('name') and i.has_key('link')
-
-class TestControllerConfiguration():
-    def setUp(self):
-	self.c = 'share'
-	self.a = 'index'
-	self.conf = None
-	
+#class TestSambaServerStatus():
+#    def test_get_samba_server_status(self):
+#	status = get_samba_server_status()
+#        assert status == "up" or status == "down", 'Status should always be "up" or "down"'
+#
+#class TestDashboardConfiguration():
+#    def setUp(self):
+#	self.d = DashboardConfiguration()
+#
+#    def tearDown(self):
+#	pass
+#
+#    def test_load_config(self):
+#	self.d.load_config('index')
+#	assert len(self.d.get_layout()) > 0
+#
+#class TestSwatMessages():
+#    def setUp(self):
+#	self.m = SwatMessages()
+#
+#    def tearDown(self):
+#	pass
+#	
+#    def test_add(self):
+#	self.m.add('test message', 'cool')
+#	assert self.m.any() == True
+#	
+#    def test_clear(self):
+#	self.m.add('test message', 'cool')
+#	self.m.clean()
+#	assert self.m.any() == False
+#	
+#    def test_any(self):
+#	self.m.add('test message', 'cool')
+#	assert self.m.any() == True
+#	
+#	self.m.clean()
+#	assert self.m.any() == False
+#	
+#class TestBreadcrumbTrail():
+#    def setUp(self):
+#	self.c = ControllerConfiguration('share')	
+#	self.b = self.new_instance()
+#
+#    def new_instance(self):
+#	return BreadcrumbTrail(self.c)
+#	
+#    def test_add(self):
+#	self.b = self.new_instance()
+#	self.b.add('bread', 'share', 'index')
+#	
+#	items = self.b.get()
+#	assert len(items) > 0
+#	
+#    def test_build(self):
+#	self.b = self.new_instance()
+#	self.b.build()
+#	
+#	items = self.b.get()
+#	
+#	assert len(items) > 0
+#		
+#	for i in items:
+#	    assert i.has_key('name') and i.has_key('link')
+#
+#class TestControllerConfiguration():
+#    def setUp(self):
+#	self.c = 'share'
+#	self.a = 'index'
+#	self.conf = None
+#	
