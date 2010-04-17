a69ba75b5146ade5d11e0768214efd6f9b160cf9
Ricardo Velhote <rvelhote@gmail.com>
2010-04-17 18:59:59 +0100

    add: base help template

--- swat/controllers/help.py
+++ swat/controllers/help.py
@@ -1,16 +1,31 @@
 import logging
+import param
 
 from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
 
 from swat.lib.base import BaseController, render
 
+from swat.lib.helpers import ControllerConfiguration, BreadcrumbTrail, \
+SwatMessages
+
 log = logging.getLogger(__name__)
 
 class HelpController(BaseController):
-
+    """ """
+    def __init__(self):
+        """ """
+        me = request.environ['pylons.routes_dict']['controller']
+        action = request.environ['pylons.routes_dict']['action']
+        
+        c.config = ControllerConfiguration(me, action)
+        
+        c.breadcrumb = BreadcrumbTrail(c.config)
+        c.breadcrumb.build()
+        
+        c.samba_lp = param.LoadParm()
+        c.samba_lp.load_default()
+        
     def index(self):
-        # Return a rendered template
-        #return render('/help.mako')
-        # or, return a response
-        return 'Hello World'
+        """ """
+        return render('/default/derived/help.mako')
