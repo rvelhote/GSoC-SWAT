8beb3a2d3f21af89cc7c4039763e1d1a27f175cf
Ricardo Velhote <rvelhote@gmail.com>
2010-04-17 18:50:43 +0100

    add: stub help controller

---
+++ swat/controllers/help.py
@@ -0,0 +1,16 @@
+import logging
+
+from pylons import request, response, session, tmpl_context as c
+from pylons.controllers.util import abort, redirect_to
+
+from swat.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
+
+class HelpController(BaseController):
+
+    def index(self):
+        # Return a rendered template
+        #return render('/help.mako')
+        # or, return a response
+        return 'Hello World'
---
+++ swat/tests/functional/test_help.py
@@ -0,0 +1,7 @@
+from swat.tests import *
+
+class TestHelpController(TestController):
+
+    def test_index(self):
+        response = self.app.get(url(controller='help', action='index'))
+        # Test response...
