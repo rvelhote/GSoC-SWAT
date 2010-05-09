ecde25dbb02b1950f2766bc88d823763a96d6e0e
Ricardo Velhote <rvelhote@gmail.com>
2010-05-09 12:07:04 +0100

    add: action stubs

--- swat/controllers/account.py
+++ swat/controllers/account.py
@@ -1,3 +1,18 @@
+#
+# Account Management Controller file for SWAT
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#   
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#   
+# You should have received a copy of the GNU General Public License
+# 
 import logging
 
 from pylons import request, response, session, tmpl_context as c
@@ -8,9 +23,11 @@ from swat.lib.base import BaseController, render
 log = logging.getLogger(__name__)
 
 class AccountController(BaseController):
-
     def index(self):
-        # Return a rendered template
-        #return render('/account.mako')
-        # or, return a response
-        return 'Hello World'
+        return 'User and Group Management'
+    
+    def user(self):
+        return "User Management"
+    
+    def group(self):
+        return "Group Management"
