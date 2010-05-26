72fecb7a2fc96b404b370dd0ef051d108c1f1517
Ricardo Velhote <rvelhote@gmail.com>
2010-05-27 00:29:43 +0100

    add: more routing options to handle subactions (specially, for now, in the Accounts Controller

--- swat/config/routing.py
+++ swat/config/routing.py
@@ -32,6 +32,8 @@ def make_map():
         map.connect('/', controller='error', action='no_libs')
 
     map.connect('/{controller}/{action}')
+    map.connect('/{controller}/{action}/{subaction}')
     map.connect('share_action', '/share/{action}/{name}', controller='share')
+    map.connect('account_action', '/{controller}/{action}/{subaction}/{id}', controller='account')
 
     return map
