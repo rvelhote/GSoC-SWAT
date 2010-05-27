6e10f2d63062f079ab0c2a07e1d04a6ed8b1c733
Ricardo Velhote <rvelhote@gmail.com>
2010-05-28 00:46:40 +0100

    fix: routing fixed again. subaction without ID was conflicting with adding something new

--- swat/config/routing.py
+++ swat/config/routing.py
@@ -32,8 +32,8 @@ def make_map():
         map.connect('/', controller='error', action='no_libs')
 
     map.connect('/{controller}/{action}')
+    map.connect('/{controller}/{action}/{subaction}', subaction='add')
     map.connect('share_action', '/{controller}/{action}/{name}', controller='share')
     map.connect('account_action', '/{controller}/{action}/{subaction}/{id}', controller='account')
-    map.connect('/{controller}/{action}/{subaction}')
 
     return map
