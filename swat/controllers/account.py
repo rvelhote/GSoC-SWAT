257e1ed60fc9fb90adc4bfc28f0dbd6c00ea3927
Ricardo Velhote <rvelhote@gmail.com>
2010-05-28 00:32:13 +0100

    add: group "internal router" chg: cleanup old unused methods

--- swat/controllers/account.py
+++ swat/controllers/account.py
@@ -79,15 +79,9 @@ class AccountController(BaseController):
         return render('/default/derived/account.mako')
     
     def user(self, subaction="index", id=-1):
-        id = int(id)
-        c.user_list = self.__manager.user_list
-        
-        c.list_users = True
-        c.list_groups = False
-        
+        id = int(id)        
         user_manager = UserManager(self.__manager)
-        template = "/default/derived/account.mako"
-        
+
         is_new = False
         
         if id == -1:
@@ -117,7 +111,7 @@ class AccountController(BaseController):
                 message = _("Sucessfuly saved the User with the ID %s" % (id))
             else:
                 type = "critical"
-                cause = ""
+                cause = _("Unkown Reason")
                 
                 if user_manager.has_message():
                     cause = user_manager.get_message()
@@ -142,66 +136,115 @@ class AccountController(BaseController):
                 message = _("Sucessfuly deleted the User with the ID %s" % (id))
             else:
                 type = "critical"
-                message = _("Error deleting the User with the ID %s" % (id))
+                cause = _("Unkown Reason")
+                
+                if user_manager.has_message():
+                    cause = user_manager.get_message()
+                
+                message = _("Error deleting the User with the ID %s  " % (id, cause))
                 
             SwatMessages.add(message, type)
             redirect_to(controller='account', action='user')
+            
+        ##
+        ## 
+        ##
+        else:
+            c.user_list = self.__manager.user_list        
+            c.list_users = True
+            c.list_groups = False
+            template = "/default/derived/account.mako"
 
         return render(template)
     
-    def group(self):
-        #c.group_list = self.__manager.group_list
-        #
-        #c.list_users = False
-        #c.list_groups = True
-        #
-        #return render('/default/derived/account.mako')
-        pass
+    def group(self, subaction="index", id=-1):
+        id = int(id)        
+        group_manager = GroupManager(self.__manager)
+
+        is_new = False
         
-    def edit(self, id, type, is_new = False):
-        #if type == "user":
-        #    c.p = ParamConfiguration('user-account-parameters')
-        #    
-        #
-        #        
-        #    return render('/default/derived/edit-user-account.mako')
-        #    
-        #elif type == "group":
-        #    c.p = ParamConfiguration('group-parameters')
-        #    
-        #    if not is_new:
-        #        c.group = self.__manager.fetch_group(id)
-        #    else:
-        #        c.group = Group("", "", -1)
-        #
-        #    return render('/default/derived/edit-group.mako')
-        #else:
-        #    message = _("You did not specify the type of account you want to edit")
-        #    SwatMessages.add(message, "critical")
-        #    redirect_to(controller='account', action='index')
-        pass
+        if id == -1:
+            is_new = True
             
-    def edituser(self):
-        #id = int(request.params.get("id", -1))
-        #return self.edit(id, "user", False)
-        pass
-    
-    def editgroup(self):
-        #id = int(request.params.get("id", -1))
-        #return self.edit(id, "group", False)
-        pass
-        
-    def adduser(self):
-        #id = int(request.params.get("id", -1))
-        #return self.edit(id, "user", True)
-        pass
-    
-    def addgroup(self):
-        #id = int(request.params.get("id", -1))
-        #return self.edit(id, "group", True)
-        pass
+        ##
+        ## Edit a Group
+        ##
+        if subaction == "edit" or subaction == "add":
+            c.p = ParamConfiguration('group-parameters')
+            c.group = group_manager.edit(id, is_new)
+
+            if c.group is not None:
+                template = "/default/derived/edit-group.mako"
+            else:
+                type = "critical"
+                cause = _("Unkown Reason")
+                
+                if user_manager.has_message():
+                    cause = user_manager.get_message()
+                    
+                message = _("Unable to get Group to edit - %s", (cause))
+                SwatMessages.add(message, type)
+
+        ##
+        ## Save the changes made to a Group
+        ##
+        elif subaction == "save" or subaction == "apply":
+            saved = group_manager.save(id, is_new)
+            
+            if saved:
+                type = "cool"
+                message = _("Sucessfuly saved the Group with the ID %s" % (id))
+            else:
+                type = "critical"
+                cause = _("Unkown Reason")
+                
+                if group_manager.has_message():
+                    cause = group_manager.get_message()
+                
+                message = _("Error saving the Group with the ID %s: %s" % (id, cause))
+                
+            SwatMessages.add(message, type)
+            
+            if subaction == "save":
+                redirect_to(controller='account', action='group')
+            elif subaction == "apply":
+                redirect_to("account_action", action='group', subaction='edit', id=id)
+            
+        ## 
+        ## Remove a Certain Group
+        ## 
+        elif subaction == "remove":
+            removed = group_manager.remove(id)
+            
+            if removed:
+                type = "cool"
+                message = _("Sucessfuly deleted the Group with the ID %s" % (id))
+            else:
+                type = "critical"
+                cause = _("Unkown Reason")
+                
+                if user_manager.has_message():
+                    cause = user_manager.get_message()
+                
+                message = _("Error deleting the Group with the ID %s - %s" % (id, cause))
+                
+            SwatMessages.add(message, type)
+            redirect_to(controller='account', action='user')
+        ##
+        ## 
+        ##
+        else:        
+            c.group_list = self.__manager.group_list
+            
+            c.list_users = False
+            c.list_groups = True
+            
+            template = '/default/derived/account.mako'
+            
+        return render(template)
         
     def save(self):
+        """ """
         id = request.params.get("id", -1)
         
         action = request.environ['pylons.routes_dict']['action']
@@ -211,167 +254,23 @@ class AccountController(BaseController):
         if type == "user":
             self.user(action, id)
         elif type == "group":
-            pass
-        
-        #action = request.environ['pylons.routes_dict']['action']
-        #
-        #task = request.params.get("task", "").strip().lower()
-        #type = request.params.get("type", "").strip().lower()
-        #
-        #is_new = False
-        #
-        #if task.startswith("add"):
-        #    is_new = True
-        #
-        #if type == "group":
-        #    id = int(request.params.get("id", -1))
-        #    name = request.params.get("group_name", "")
-        #    description = request.params.get("group_description", "")
-        #
-        #    group = Group(name, description, id)
-        #    
-        #    if is_new:
-        #        self.__manager.add_group(group)
-        #        id = group.rid
-        #    else:
-        #        self.__manager.update_group(group)
-        #    
-        #    if action == "apply" or action == "save":
-        #        message = _("saved group id %d" % (id))
-        #        SwatMessages.add(message)
-        #        redirect_to(controller='account', action='editgroup', id = id)
-        #elif type == "user":
-        #    id = int(request.params.get("id", -1))
-        #    username = request.params.get("account_username", "")
-        #    fullname = request.params.get("account_fullname", "")
-        #    description = request.params.get("account_description", "")
-        #    
-        #    password = request.params.get("account_password", "")
-        #    confirm_password = request.params.get("confirm_password", "")
-        #    
-        #    if len(password) > 0 and password != confirm_password:
-        #        message = _("Passwords do not match")
-        #        SwatMessages.add(message)
-        #        redirect_to(controller='account', action='edituser', id = id)
-        #
-        #    #
-        #    # FIXME too complicated
-        #    #
-        #    must_change_password = request.params.get("account_must_change_password", "no")
-        #    if must_change_password == "yes":
-        #        must_change_password = True
-        #    else:
-        #        must_change_password = False
-        #        
-        #    cannot_change_password = request.params.get("account_cannot_change_password", "no")
-        #    if cannot_change_password == "yes":
-        #        cannot_change_password = True
-        #    else:
-        #        cannot_change_password = False
-        #
-        #    password_never_expires = request.params.get("account_password_never_expires", "no")
-        #    if password_never_expires == "yes":
-        #        password_never_expires = True
-        #    else:
-        #        password_never_expires = False
-        #    
-        #    account_disabled = request.params.get("account_account_disabled", "no")
-        #    if account_disabled == "yes":
-        #        account_disabled = True
-        #    else:
-        #        account_disabled = False
-        #    
-        #    account_locked_out = request.params.get("account_account_locked_out", "no")
-        #    if account_locked_out == "yes":
-        #        account_locked_out = True
-        #    else:
-        #        account_locked_out = False
-        #
-        #    group_list = []
-        #    for g in request.params.get("account_group_list", "").split(","):
-        #        for gg in self.__manager.group_list:
-        #            if gg.name == g.strip():
-        #                group_list.append(gg)
-        #
-        #    profile_path = request.params.get("account_profile_path", "")
-        #    logon_script = request.params.get("account_logon_script", "")
-        #    homedir_path = request.params.get("account_homedir_path", "")
-        #    map_homedir_drive = request.params.get("account_map_homedir_drive", "")
-        #
-        #    user = User(username, fullname, description, id)
-        #    
-        #    user.password = password
-        #    user.must_change_password = must_change_password
-        #    
-        #    user.cannot_change_password = cannot_change_password
-        #    user.password_never_expires = password_never_expires
-        #    user.account_disabled = account_disabled
-        #    
-        #    user.account_locked_out = account_locked_out
-        #    user.group_list = group_list
-        #    user.profile_path = profile_path
-        #    
-        #    user.logon_script = logon_script
-        #    user.homedir_path = homedir_path
-        #    user.map_homedir_drive = int(map_homedir_drive)
-        #
-        #    if is_new:
-        #        self.__manager.add_user(user)
-        #        id = user.rid
-        #    else:
-        #        self.__manager.update_user(user)
-        #    
-        #    if action == "apply" or action == "save":
-        #        message = _("saved user id %d" % (id))
-        #        SwatMessages.add(message)
-        #        redirect_to(controller='account', action='edituser', id = id)
-        #
-        #return "Not Implemented"
-        pass
+            self.group(action, id)
     
     def apply(self):
+        """ """
         self.save()
 
     def cancel(self):
+        """ """
         type = request.params.get("type", "").strip().lower()
         
         message = _("Editing canceled. No changes were saved.")
         SwatMessages.add(message, "warning")
         
         redirect_to(controller='account', action=type)
-
-    def removegroup(self):
-        #id = int(request.params.get("id", -1))
-        #type = "cool"
-        #
-        #try:
-        #    self.__manager.delete_group(Group("", "", id))
-        #    message = _("deleted group id %d" % (str(id)))
-        #except RuntimeError:
-        #    message = _("error deleting group id %d" % (id))
-        #    type = "critical"
-        #
-        #SwatMessages.add(message, type)
-        #redirect_to(controller='account', action='group')
-        pass
-    
-    def removeuser(self):
-        #id = int(request.params.get("id", -1))
-        #type = "cool"
-        #
-        #try:
-        #    self.__manager.delete_user(User("", "", "", id))
-        #    message = _("deleted user id %d" % (id))
-        #except RuntimeError:
-        #    message = _("error deleting user id %d" % (id))
-        #    type = "critical"
-        #
-        #SwatMessages.add(message)
-        #redirect_to(controller='account', action='user')
-        pass
         
     def show_groups(self):
-        #""" """
+        """ """
         already_selected = request.params.get('as', '')
         log.debug("These are selected: " + already_selected)
         
